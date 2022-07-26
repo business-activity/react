72f6d75ff8f5f2a65b70c13d07b67690fd49a0ce
OraBenesh <orabenesh@gmail.com>
2022-07-26 17:40:37 +0300

    business for users

---
+++ business-activity/src/components/Business.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+async function getBusiness() {
+    let businessList = [];
+    try {
+        const res = await axios.get("https://meetings-test.herokuapp.com/business");
+        businessList = res.data.map((item) => {
+            let b = {
+                id:item.id,
+                userid: item.userid,
+                ownersName: item.businessDetails.ownersName,
+                businessName: item.businessDetails.businessName,
+                services: item.businessDetails.services
+            }
+            return b;
+        })
+    } catch (err) {
+        console.log(err);
+    }
+    return businessList;
+}
+export default async function BusinessForUsers() {
+    const businessList = await getBusiness();
+    const navigate=useNavigate();
+    return (
+        <>
+            {
+                businessList.map((business) => {
+                    <div onClick={navigate('./BusinessDetails',{id:business.id})}>
+                        <div>
+                            {business.businessName}
+                        </div>
+                        <div>
+                            by {business.ownersName}
+                        </div>
+                    </div>
+                })
+            }
+        </>
+    );
+}
