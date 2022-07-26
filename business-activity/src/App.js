dd3f45539852c52dd2d39e8c9522bd78a1997c8e
naamafrank1 <naama0556772275@gmail.com>
2022-07-26 18:17:51 +0300

    details af selected business

--- business-activity/src/App.js
+++ business-activity/src/App.js
@@ -1,17 +1,21 @@
 import * as React from "react";
 import BusinessForUsers from "./components/Business";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BusinessDetails from "./components/BusinessDetails";
 
 function App() {
   return (
+    <>
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<BusinessForUsers />} />
+          <Route path="./BusinessDetails" element={<BusinessDetails />} />
           {/* <Route path="/BusinessDetails" element={<BusinessDetails/>} /> */}
         </Routes>
-      </Router>
+      </Router>     
     </div>
+   </>
   );
 }
 
--- business-activity/src/components/BusinessDetails.js
+++ business-activity/src/components/BusinessDetails.js
@@ -5,14 +5,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function BusinessDetails() {
   return (
     <>
-       <Typography sx={{ textAlign:'center'}} variant="h3" gutterBottom component="div">
-   
-      </Typography>
-    <Card sx={{ maxWidth: 600,textAlign:'center',alignItems:'center',marginLeft:'33%',marginTop:5}}>
+    
+    <Card sx={{ maxWidth: 600,alignItems:'center',marginLeft:'33%',marginTop:5}}>
       <CardMedia
         component="img"
        height="400"
@@ -21,14 +21,24 @@ export default function BusinessDetails() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-        business details
+        name of business
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-          Lizards are a widespread group of squamate reptiles, with over 6,000
+          
+        <Typography variant="h6" gutterBottom component="div">
+        duration of meeting:
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+        cost:
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+        address:
+        </Typography>
+        <Typography variant="h6" gutterBottom component="div">
+        service name
+        </Typography>
+
+        
      
         </Typography>
       </CardContent>
