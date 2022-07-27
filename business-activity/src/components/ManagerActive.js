328039d1b910bf958ab9645b3fea0f8609440b33
OraBenesh <orabenesh@gmail.com>
2022-07-27 15:52:27 +0300

    location

--- business-activity/src/components/ManagerActive.js
+++ business-activity/src/components/ManagerActive.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+
 
 export default function componentName(props) {
+  const location = useLocation();
+  const form = location.state;
+  let business;
   try {
-   // const userId = '2c4db4bf-9145-491f-8990-7c811fbcae61';
-    const res = axios.get(`https://meetings-test.herokuapp.com/business/${props.managerId}`)
-      .then(() => { 
-        console.log(res)
-       })
-      .catch((err) => console.log(err))
+    const res = axios.get(`https://meetings-test.herokuapp.com/business/${form.id}`).then(() => {
+      business = res.data;
+      debugger
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
+  // try {
+  //  // const userId = '2c4db4bf-9145-491f-8990-7c811fbcae61';
+  //   const res = axios.get(`https://meetings-test.herokuapp.com/business/${props.managerId}`)
+  //     .then(() => { 
+  //       console.log(res)
+  //      })
+  //     .catch((err) => console.log(err))
+  // } catch (err) {
+  //   console.log(err);
+  // }
   return (
   
   <>
