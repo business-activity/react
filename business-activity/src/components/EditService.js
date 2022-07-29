d47ecab54e725962e03de2c130dc36a6537a4c8b
naamafrank1 <naama0556772275@gmail.com>
2022-07-29 17:53:58 +0300

    start ref

--- business-activity/src/components/EditService.js
+++ business-activity/src/components/EditService.js
@@ -1,37 +1,105 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useLocation, useNavigate,useRef } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 
 
+// PUT /service/:id - update service
+// body: {
+// 	service: json object
+// }
 
 export default function EditService() {
     const [services, setServices] = useState([]);
+    const [updaeService, setUpdateService] = useState()
     const location = useLocation();
     const form = location.state;
-     const deleteServices =async () => {
-    //     try {
-    //         await axios.delete(`https://meetings-test.herokuapp.com/service/${form.id}`)
-    //             .then((res) => {
-    //                 console.log(res)
-    //                 alert("your Services deleted!!!")
-                  
-    //             })
-    //             .catch((err) => {
-    //                 debugger
-    //                 console.log(err);
-    //             })
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-     }
-    const updateServices = () => {
+    const inputId = useRef();
+    const inputName = useRef();
+    const inputNum = useRef();
+    const inputDuration = useRef();
+    const inputCost = useRef();
+    const inputOpeningHours = useRef()
+    const inputNumber = useLocation();
+    const inputCity = location.state;
+    const inputStreet = useRef();
 
+
+    const deleteServices = async () => {
+
+        //     try {
+        //         await axios.delete(`https://meetings-test.herokuapp.com/service/${form.id}`)
+        //             .then((res) => {
+        //                 console.log(res)
+        //                 alert("your Services deleted!!!")
+
+        //             })
+        //             .catch((err) => {
+        //                 debugger
+        //                 console.log(err);
+        //             })
+        //     } catch (err) {
+        //         console.log(err);
+        //     }
+    }
+    const updateServices = async () => {
+
+
+
+        const data = {
+
+          
+            "numOfMeetings": inputNum.current?.value,
+            "durationOfMeeting": inputDuration.current?.value,
+            "cost": inputCost.current?.value,
+            "OpeningHours": inputOpeningHours.current?.value,
+            "address": {
+                "city": inputCity.current?.value,
+                "street": inputStreet.current?.value,
+                "number": inputNumber.current?.value,
+
+            }
+        }
+        console.log(data);
+
+
+
+
+
+
+
+        //  debugger
+        // try {
+        //     debugger
+        //     await axios.put(`https://meetings-test.herokuapp.com/service/${form.id},${{"service":{
+        //         "numOfMeetings": 4,
+        //         "durationOfMeeting": "1 hour",
+        //         "cost": "200$",
+        //         "OpeningHours": "10:00-21:30",
+        //         "address": {
+        //             "city": "bnei-brak",
+        //             "street": "pardo",
+        //             "number": 2
+        //         }
+        //     }
+        // }}`)
+        //         .then((res) => {
+
+        //             console.log(res.data)
+
+        //         })
+        //         .catch((err) => {
+        //             debugger
+        //             console.log(err);
+        //         })
+        // } catch (err) {
+        //     console.log(err);
+        // }
     }
     useEffect(() => {
         async function getBusiness() {
@@ -43,7 +111,7 @@ export default function EditService() {
                         debugger;
                         console.log(res.data)
                         let tempList = {
-                         
+
                             id: res.data.id,
                             name: res.data.name,
                             num: res.data.numOfMeetings,
@@ -77,102 +145,111 @@ export default function EditService() {
 
             {services &&
                 <Card sx={{ minWidth: 275, textAlign: 'center' }}>
-                    <Typography sx={{ textAlign: 'center', color: '#edcf3f' }} gutterBottom variant="h4" component="div">
-                        edit your service
-                    </Typography>
-                    <CardContent>
-                        <div>
-                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
-                                service name:
-                            </Typography>
-                            <TextField
-                                id="outlined-textarea"
-                                label={services.name}
-                                placeholder={services.name}
-                                multiline
-                            />
-                        </div>
-
-                        <div>
-                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
-                                number of meeting:
-                            </Typography>
-                            <TextField
-                                id="outlined-textarea"
-                                label={services.num}
-                                placeholder={services.num}
-                                multiline
-                            />
-                        </div>
-                        <div>
-                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
-                                duration of meeting
-                            </Typography>
-                            <TextField
-                                id="outlined-textarea"
-                                label={services.duration}
-                                placeholder={services.duration}
-                                multiline
-                            />
-                        </div>
-                        <div>
-                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
-                                open in Hours:
-                            </Typography>
-                            <TextField
-                                id="outlined-textarea"
-                                label={services.openingHours}
-                                placeholder={services.openingHours}
-                                multiline
-                            />
-                        </div>
-                        <div>
-                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
-                                cost:
-                            </Typography>
-                            <TextField
-                                id="outlined-textarea"
-                                label={services.cost}
-                                placeholder={services.cost}
-                                multiline
-                            />
-                        </div>
-
-                        <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h6" component="div">
-                            address:
+                    <form >
+                        <Typography sx={{ textAlign: 'center', color: '#edcf3f' }} gutterBottom variant="h4" component="div">
+                            edit your service
                         </Typography>
+                        <CardContent>
+                            <div>
+                                <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
+                                    service name:
+                                </Typography>
+                                <TextField
+                                    ref={inputName}
+                                    id="outlined-textarea"
+                                    label={services.name}
+                                    placeholder={services.name}
+                                    multiline
+                                />
+                            </div>
 
-                        <TextField
-                            sx={{ margin: '1%' }}
-                            id="outlined-textarea"
-                            label={services.city}
-                            placeholder={services.city}
-                            multiline
-                        />
+                            <div>
+                                <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
+                                    number of meeting:
+                                </Typography>
+                                <TextField
+                                    ref={inputNum}
+                                    id="outlined-textarea"
+                                    label={services.num}
+                                    placeholder={services.num}
+                                    multiline
+                                />
+                            </div>
+                            <div>
+                                <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
+                                    duration of meeting
+                                </Typography>
+                                <TextField
+                                    ref={inputDuration}
+                                    id="outlined-textarea"
+                                    label={services.duration}
+                                    placeholder={services.duration}
+                                    multiline
+                                />
+                            </div>
+                            <div>
+                                <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
+                                    open in Hours:
+                                </Typography>
+                                <TextField
+                                    ref={inputOpeningHours}
+                                    id="outlined-textarea"
+                                    label={services.openingHours}
+                                    placeholder={services.openingHours}
+                                    multiline
+                                />
+                            </div>
+                            <div>
+                                <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h7" component="div">
+                                    cost:
+                                </Typography>
+                                <TextField
+                                    ref={inputCost}
+                                    id="outlined-textarea"
+                                    label={services.cost}
+                                    placeholder={services.cost}
+                                    multiline
+                                />
+                            </div>
 
-                        <TextField
-                            sx={{ margin: '1%' }}
-                            id="outlined-textarea"
-                            label={services.street}
-                            placeholder={services.street}
-                            multiline
-                        />
+                            <Typography sx={{ textAlign: 'center' }} gutterBottom variant="h6" component="div">
+                                address:
+                            </Typography>
 
-                        <TextField
-                            sx={{ margin: '1%' }}
-                            id="outlined-textarea"
-                            label={services.numberM}
-                            placeholder={services.numberM}
-                            multiline
-                        />
+                            <TextField
+                                ref={inputCity}
+                                sx={{ margin: '1%' }}
+                                id="outlined-textarea"
+                                label={services.city}
+                                placeholder={services.city}
+                                multiline
+                            />
+
+                            <TextField
+                                ref={inputStreet}
+                                sx={{ margin: '1%' }}
+                                id="outlined-textarea"
+                                label={services.street}
+                                placeholder={services.street}
+                                multiline
+                            />
+
+                            <TextField
+                                ref={inputNumber}
+                                sx={{ margin: '1%' }}
+                                id="outlined-textarea"
+                                label={services.numberM}
+                                placeholder={services.numberM}
+                                multiline
+                            />
 
 
 
-                    </CardContent>
+                        </CardContent>
 
-                    <Button onClick={updateServices()} sx={{ margin: '2%' }} variant="contained">update your details</Button>
-                    <Button onClick={deleteServices()} sx={{ margin: '2%' }} variant="contained">delete your services</Button>
-
+                        <Button onClick={updateServices()} sx={{ margin: '2%' }} variant="contained">update your details</Button>
+                        <Button onClick={deleteServices()} sx={{ margin: '2%' }} variant="contained">delete your services</Button>
+                    </form>
                 </Card>}
         </>
     );
