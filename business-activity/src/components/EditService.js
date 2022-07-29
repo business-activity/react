9cb1ce5e7d8a14dd4a11faabccf42edb0dfbc39f
naamafrank1 <naama0556772275@gmail.com>
2022-07-29 15:58:13 +0300

    fix the kod

--- business-activity/src/components/EditService.js
+++ business-activity/src/components/EditService.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate,useRef } from 'react-router-dom';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
-import { flexbox } from '@mui/system';
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
+
+
 
 export default function EditService() {
     const [services, setServices] = useState([]);
